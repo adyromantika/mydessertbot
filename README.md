56dac5bc65d7e8f5bbb7574e742e62253b056bb7
Ady Romantika <ady.romantika@gmail.com>
2019-11-04 22:35:11 +0800

    Working bot

---
+++ README.md
@@ -0,0 +1,16 @@
+# Telegram Dessert Bot
+
+A **very** simple Telegram bot written in Node.js
+
+## Development
+
+1. Create a bot by contacting [@botfather](https://t.me/botfather) and get your bot token
+1. Export the token into the environment variable `TELEGRAM_BOT_TOKEN`
+1. Run `npm install`
+1. Execute the code
+
+(For development purpose it is much simpler to run in polling mode)
+
+## See it in action
+
+[@mydessertbot](https://t.me/mydessertbot)
--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-const dessertGenerator = require('random-dessert-generator')
+const TelegramBot = require('node-telegram-bot-api');
+const dessertGenerator = require('random-dessert-generator');
 
-var dessert = dessertGenerator.generateDessert()
-console.info(dessert)
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  var keys = [];
+  keys.push({text: "dessert", callback_data: "dessert"});
+  keys.push({text: "yummy dessert", callback_data: "yummy"});
+  var options = { reply_markup: ({inline_keyboard: [ keys ]}) };
+  bot.sendMessage(chatId, "Your choices:", options);
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  console.log(callbackQuery);
+  const chatId = callbackQuery.message.chat.id;
+  var dessert;
+  switch (callbackQuery.data) {
+    case 'dessert':
+      dessert = dessertGenerator.generateDessert();
+      break;
+    case 'yummy':
+      dessert = dessertGenerator.generateYummyDessert();
+      break;
+  }
+  bot.answerCallbackQuery(callbackQuery.id, { text: dessert });
+  bot.sendMessage(chatId, dessert);
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/dessert/, (msg) => {
+  const chatId = msg.chat.id;
+  const dessert = dessertGenerator.generateDessert();
+
+  bot.sendMessage(chatId, dessert);
+});
+
+bot.onText(/\/yummy/, (msg) => {
+  const chatId = msg.chat.id;
+  const dessert = dessertGenerator.generateYummyDessert();
+
+  bot.sendMessage(chatId, dessert);
+});
